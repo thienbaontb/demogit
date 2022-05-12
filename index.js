27b5331ef097ce0c61c1639d4795183933680b82
baont <baont@vnpt.vn>
2022-05-12 11:08:15 +0700

    Revert "update"
    This reverts commit 5957ebcd5eeb1fcdefb1c81c41b2d8d03a26ec4e.

--- index.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var app = express();
-var server = require("http").createServer(app);
-server.listen(process.env.PORT || 3000);
-
-app.get("/", function(req, res){
-	res.send("dbsjhbfsndk");
-});
